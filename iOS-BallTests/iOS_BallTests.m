89d1ee7c2f36cee8494a3e05c6774fd533cb9eca
Abhijit Joshi <joshi1974@gmail.com>
2014-05-03 00:41:55 -0400

    Initial Commit

---
+++ iOS-BallTests/iOS_BallTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_BallTests.m
+//  iOS-BallTests
+//
+//  Created by Abhijit Joshi on 5/3/14.
+//  Copyright (c) 2014 Misha software solutions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_BallTests : XCTestCase
+
+@end
+
+@implementation iOS_BallTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
